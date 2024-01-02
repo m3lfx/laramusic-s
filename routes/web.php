902f65d94772ad2606c7ddabbe66bfd8f205007f
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-02 14:26:16 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('my/page', function () {
+    return 'Hello world!';
+    });
+    
+   
