adaaedb80b41bfa1e7d14da0a1a0c67e4a749cc3
rommel dalisay <rommelmdalisay@gmail.com>
2024-01-11 11:04:16 +0800

    artist store edit

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Artist;
 use View;
+use Redirect;
 
 class ArtistController extends Controller
 {
@@ -19,13 +20,26 @@ class ArtistController extends Controller
         return View::make('artist.create');
     }
 
-    public function store() {
-        return 'from store';
+    public function store(Request $request) {
+        $name = $request->name;
+        $country = $request->country;
+        $img_path = $request->img_path;
+        
+        $artist = new Artist();
+        $artist->name = $name;
+        $artist->country = $country;
+        $artist->img_path = $img_path;
+        $artist->save();
+        // dd($artist);
+        
+        return Redirect::to('artist');
     }
 
     public function edit($id) {
-        dd($id);
-        return 'from edit';
+        $artist = Artist::find($id);
+
+        // dd($artist);
+        return View::make('artist.edit', compact('artist'));
     }
 
     public function update(){
--- resources/views/artist/edit.blade.php
+++ resources/views/artist/edit.blade.php
@@ -5,16 +5,16 @@
         
         <div class="form-group">
             <label for="exampleInputEmail1">Artist Name</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Artist Name" name="name">
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Artist Name" name="name" value={{$artist->name}}>
 
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">country</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="artist country" name="country">
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="artist country" name="country" value={{$artist->country}}>
         </div>
         <div class="form-group">
-            <label for="image">country</label>
-            <input type="text" class="form-control" name="img_path">
+            <label for="image">image</label>
+            <input type="text" class="form-control" name="img_path" value={{$artist->img_path}}>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
